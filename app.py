029ebcf15c2922341ff2de8b9dc52413d9fc8b0f
Apurv-TA <apurv.master@tigeranalytics.com>
2022-04-18 09:12:34 +0530

    Modified some files and added requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import os
 from flask import Flask, render_template, request, redirect, send_file
 
 from s3 import list_buckets, created_folder
 
 app = Flask(__name__)
 BUCKET = "tiger-mle-pg"
+PREFIX = "home/apurv.master@tigeranalytics.com"
+
 
 @app.route("/")
 def entry_point():
@@ -19,8 +20,10 @@ def storage():
 
 @app.route("/files")
 def myfiles():
-    f = created_folder()
+    f = created_folder(bucket=BUCKET, prefix=PREFIX)
     return render_template("storage.html", contents=f)
 
+
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5003, debug=True)
+    # app.run(host='0.0.0.0', port=5003, debug=True)
+    app.run()
--- s3.py
+++ s3.py
@@ -2,8 +2,8 @@ import boto3
 
 
 def list_buckets():
-    session = boto3.Session(profile_name="mle-intern-dev")
-    s3 = session.client('s3')
+    # session = boto3.Session(profile_name="mle-intern-dev")
+    s3 = boto3.client('s3')
 
     existing_buckets = []
     for bucket in s3.list_buckets()["Buckets"]:
@@ -12,17 +12,12 @@ def list_buckets():
     return existing_buckets
 
 
-def created_folder():
-    session = boto3.Session(profile_name="mle-intern-dev")
-    s3 = session.resource("s3")
-    my_bucket = s3.Bucket("tiger-mle-pg")
+def created_folder(bucket, prefix):
+    s3 = boto3.resource("s3")
+    my_bucket = s3.Bucket(bucket)
 
     var = []
 
-    for object_summary in my_bucket.objects.filter(Prefix="home/apurv.master@tigeranalytics.com"):
+    for object_summary in my_bucket.objects.filter(Prefix=prefix):
         var.append(object_summary.key)
     return var
-
-
-# if __name__ == "__main__":
-#     print(list_buckets())
