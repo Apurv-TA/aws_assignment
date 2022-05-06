44130180bcff8548bb609de101f2652e5fbd5497
Ubuntu <ubuntu@ip-172-31-30-228.ec2.internal>
2022-05-06 06:00:08 +0000

    Added docker files and updated environment

--- s3_ec2/app.py
+++ s3_ec2/app.py
@@ -26,4 +26,5 @@ def myfiles():
 
 if __name__=="__main__":
     # app.run(host='0.0.0.0', port=5003, debug=True)
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
